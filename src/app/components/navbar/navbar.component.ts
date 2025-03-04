e35d874b114f2f0413792b54be7456f67afb4f18
Sharier Himu <himusharier@gmail.com>
2025-03-04 20:36:15 +0600

    login and register separated from navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { RegisterComponent } from "../register/register.component";
+import { LoginComponent } from "../login/login.component";
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, RegisterComponent, LoginComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
