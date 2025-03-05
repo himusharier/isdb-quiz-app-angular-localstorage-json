3833cc3221ac69e2a6976ff9abac5f88be03bbfd
Sharier Himu <himusharier@gmail.com>
2025-03-05 13:35:44 +0600

    profile page updated

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,8 @@ import { NavbarComponent } from "../components/navbar/navbar.component";
 import { HeaderComponent } from "../components/header/header.component";
 import { HeaderTitleService } from '../services/header-title/header-title.service';
 import { Title } from '@angular/platform-browser';
+import { LoggedinUserService } from '../services/loggedin-user/loggedin-user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -11,10 +13,14 @@ import { Title } from '@angular/platform-browser';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent implements OnInit {
+  loggedinUserName: string = "";
+  loggedinUserEmail: string = "";
 
   constructor(
     private headerTitleService: HeaderTitleService,
-    private titleService: Title
+    private titleService: Title,
+    private loggedinUserService: LoggedinUserService,
+    private router: Router
   ) {}
 
   updateHeaderTitleValue(givenTitle: string) {
@@ -24,5 +30,15 @@ export class ProfileComponent implements OnInit {
   
   ngOnInit(): void {
     this.updateHeaderTitleValue("Profile");
+
+    if (!this.loggedinUserService.isLoggedIn()) {
+      this.router.navigate(['/home']);
+
+    } else {
+      const loggedInUser = this.loggedinUserService.getLoggedInUser();
+      this.loggedinUserName = loggedInUser ? loggedInUser.userName : ''; 
+      this.loggedinUserEmail = loggedInUser ? loggedInUser.userEmail : ''; 
+    }
+
   }
 }
