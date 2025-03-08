e930f38d77b683fc3e49fad8b77389e05e27f904
Sharier Himu <himusharier@gmail.com>
2025-03-08 19:30:41 +0600

    quiz list shown in quizzes page

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -3,14 +3,17 @@ import { NavbarComponent } from "../components/navbar/navbar.component";
 import { HeaderComponent } from "../components/header/header.component";
 import { HeaderTitleService } from '../services/header-title/header-title.service';
 import { Title } from '@angular/platform-browser';
+import { Quizzes } from '../model/quizzes.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quiz',
-  imports: [NavbarComponent, HeaderComponent],
+  imports: [NavbarComponent, HeaderComponent, CommonModule],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.css'
 })
 export class QuizComponent implements OnInit {
+  quizzes: Quizzes[] = [];
 
   constructor(
     private headerTitleService: HeaderTitleService,
@@ -24,5 +27,7 @@ export class QuizComponent implements OnInit {
   
   ngOnInit(): void {
     this.updateHeaderTitleValue("Quizzes");
+
+    this.quizzes = JSON.parse(localStorage.getItem('quizzes') || '[]');
   }
 }
