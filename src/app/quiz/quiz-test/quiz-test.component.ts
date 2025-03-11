5c94bda91b3dcbfc3c28f509ce0eec4a0f6cccb9
Sharier Himu <himusharier@gmail.com>
2025-03-11 07:40:00 +0600

    quiz test component added

---
+++ src/app/quiz/quiz-test/quiz-test.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-test',
+  imports: [],
+  templateUrl: './quiz-test.component.html',
+  styleUrl: './quiz-test.component.css'
+})
+export class QuizTestComponent {
+
+}
